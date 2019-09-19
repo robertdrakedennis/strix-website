3c37d1385bf1ed88fe1d765c3988e7fe247e5fa0
atlas <robertdennispersonal@gmail.com>
2019-09-19 04:50:50 -0400

    remove

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
 module.exports = {
     plugins: [
         require("tailwindcss"),
-        ...(process.env.NODE_ENV === `production`,
-            [
-                require("@fullhuman/postcss-purgecss")({
-                    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
-                    defaultExtractor: content =>
-                        content.match(/[A-Za-z0-9-_:/]+/g) || []
-                }),
-                require("autoprefixer"),
-                require("cssnano")
-            ])
+        require("autoprefixer"),
+        require("cssnano")
     ]
 };
